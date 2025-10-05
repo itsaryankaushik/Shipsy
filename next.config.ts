b4239c118f9076b40f4a63aaef98d037ee4cfd29
Aryan Kaushik <aryankaushik_mc22a13_55@dtu.ac.in>
2025-10-05 17:11:50 +0530

    project init

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  turbopack: {
+    // set the workspace root explicitly (you can also use a specific path)
+    root: process.cwd(),
+  },
   /* config options here */
 };
 
